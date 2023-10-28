3f5a48ac036e0823afe316d0a7e7630da513a933
AmrullohMK <amrullokh.komiljonov@gmail.com>
2023-10-28 12:32:12 +0500

    Update urls.py

--- myfirstproject/urls.py
+++ myfirstproject/urls.py
@@ -38,7 +38,4 @@ urlpatterns = [
     path("profile/",user_views.profile, name = 'profile' ),
     
     
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
